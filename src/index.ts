76f6be509348885c1a8c92f2f3274bdfafbd3fc0
bsorrentino <bartolomeo.sorrentinogmail.com>
2021-04-21 12:29:52 +0200

    update for node 12

--- src/index.ts
+++ src/index.ts
@@ -133,7 +133,7 @@ export async function run() {
                             cmdobj.parent.outdir : 
                             path.basename(pdfPath, '.pdf')
 
-  program.version('0.2.1')
+  program.version('0.3.0')
     .name('pdftools')
     .option('-o, --outdir [folder]', 'output folder')
 
