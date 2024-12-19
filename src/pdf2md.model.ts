6ae6e93246e7cf1f8053ba4609b1ef74a26bb3ff
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-12-19 20:10:24 +0100

    docs: add jsdoc

--- src/pdf2md.model.ts
+++ src/pdf2md.model.ts
@@ -1,34 +1,61 @@
 import { globals } from "./pdf2md.global"
-
-
+/**
+ * Represents a font with an optional name.
+ * @interface Font
+ * @property {string | null} name - The name of the font, or null if not available.
+ */
 export interface Font {
     name: string | null
 }
-
+/**
+ * Represents a rectangle with position and size.
+ * @property {number} x - The x-coordinate of the top-left corner of the rectangle.
+ * @property {number} y - The y-coordinate of the top-left corner of the rectangle.
+ * @property {number} width - The width of the rectangle.
+ * @property {number} height - The height of the rectangle.
+ */
 export interface Rect {
     x: number
     y: number
     width: number
     height: number
 }
-
+/**
+ * Represents an image that extends the Rect interface.
+ * @property {string} url - The URL of the image.
+ */
 export interface Image extends Rect {
     url: string
 }
-
+/**
+ * Interface representing a word with additional properties.
+ *
+ * @interface Word
+ * @extends {Rect}
+ * @property {string} text - The text content of the word.
+ * @property {string} font - The font style for the text.
+ */
 export interface Word extends Rect {
     text: string
     font: string
 }
-
+/**
+ * @typedef {function(T): T} ItemTransformer
+ * A function that takes a value of type `T` and returns a value of the same type `T`.
+ *
+ * @template T - The type of the input and output values.
+ */
 export interface ItemTransformer<T> {
     (value: T): T
 }
-
+/**
+ * @typedef {ItemTransformer<string>} TextTransformer
+ */
 type TextTransformer = ItemTransformer<string>
-
 const FILLER = ' ¶ '
-
+/**
+ * @param {TextTransformer} transformer - A function that transforms the text.
+ */
 export class EnhancedWord implements Word {
     x: number
     y: number
@@ -120,6 +147,3 @@ export class EnhancedWord implements Word {
     }
 
 }
-
-
-
