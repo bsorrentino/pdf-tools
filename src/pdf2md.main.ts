f0940bb5f21b8c23576919515882bb95d216f987
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-12-19 20:05:47 +0100

    chore: remove old comments

--- src/pdf2md.main.ts
+++ src/pdf2md.main.ts
@@ -21,10 +21,7 @@ const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
 
 
-/**
- * 
- * @param pdfPath 
- */
+
 export async function pdfToMarkdown(pdfPath: string) {
 
   try {
