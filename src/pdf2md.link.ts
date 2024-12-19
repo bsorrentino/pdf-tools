0263c9f1c6d325513cc472db4fe004b3b55ef227
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-12-19 20:00:56 +0100

    docs: add jsdoc

--- src/pdf2md.link.ts
+++ src/pdf2md.link.ts
@@ -1,9 +1,11 @@
 import { PDFPageProxy } from 'pdfjs-dist/legacy/build/pdf.js'
 import { Rect } from "./pdf2md.model"
+
 export const matchLink = <T extends Rect>(rect:T, link:PDFLink) => 
   (rect.x >= link.x1 && rect.x + rect.width <= link.x2) &&
   (rect.y >= link.y1 && rect.y + rect.height <= link.y2)
-/**
+
+  /**
  * Retrieves and processes annotations from a PDFPageProxy to return an array of PDFLink objects.
  * @param {PDFPageProxy} page - The PDFPageProxy object to process.
  * @returns {Promise<PDFLink[]>} A Promise that resolves to an array of PDFLink objects, each representing a link annotation in the page.
